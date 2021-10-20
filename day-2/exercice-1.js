bbed9910ab81d4bca2935f32e30e40960016dad1
Quentin TENGA <quentin.tenga@epita.fr>
2021-10-20 15:43:47 +0200

    jusqu'à l'exo 2

--- day-2/exercice-1.js
+++ day-2/exercice-1.js
@@ -1,3 +1,13 @@
 export const my_alpha_reverse = (str) => {
-
+    export const reverse = (str) => {
+        var res = "";
+        let i = 0;
+    
+        while (str[i] !== undefined) {
+            res = str[i] + res;
+            ++i;
+        }
+    
+        return res;
+    }
 }
