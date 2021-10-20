ca900205fd2ee5e6b70e2494e0ed75b28b908d94
Quentin TENGA <quentin.tenga@epita.fr>
2021-10-20 14:47:43 +0200

    TOUT LE DAY 1

--- day-1/exercice-7.js
+++ day-1/exercice-7.js
@@ -1,10 +1,8 @@
 export const my_is_posi_neg = (nbr) => {
     if (nbr < 0)
         return 'NEGATIF'
-    if (nbr > 0 || isNaN(nbr) || nbr == null || typeof nbr === undefined)
+    if (nbr > 0 || isNaN(nbr) || nbr === null || nbr === undefined)
         return 'POSITIF'
     if (nbr == 0)
         return 'NEUTRAL'
 }
-
-console.log(my_is_posi_neg(hel));
