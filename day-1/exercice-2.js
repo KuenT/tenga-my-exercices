db432e13e9ac1cb946938dfba76daa0a457589eb
Quentin TENGA <quentin.tenga@epita.fr>
2021-10-20 15:09:12 +0200

    TOUT LE DAY 1

--- day-1/exercice-2.js
+++ day-1/exercice-2.js
@@ -1,6 +1,19 @@
 import { my_display_alpha } from "./exercice-1.js";
 
-export const my_display_alpha_reverse = () => {
-    let rev = my_display_alpha().split('').reverse().join('');
-    return rev;
+export const reverse = (str) => {
+    var res = "";
+    let i = 0;
+
+    while (str[i] !== undefined) {
+        res = str[i] + res;
+        ++i;
+    }
+
+    return res;
 }
+
+export const my_display_alpha_reverse = () => {
+    return reverse(my_display_alpha());
+}
+
+console.log(my_display_alpha_reverse());
