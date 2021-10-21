4bd1450d4766115c8b5a29810b993b046feac5d0
Quentin TENGA <quentin.tenga@epita.fr>
2021-10-21 17:16:54 +0200

    TOUT LE DAY 2

--- day-2/exercice-6.js
+++ day-2/exercice-6.js
@@ -1,2 +1,27 @@
+import { my_display_alpha } from "../day-1/exercice-1.js";
+
 export const my_display_unicode = (arr) => {
+  var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCaseLetters = my_display_alpha();
+  var numericLetters = "0123456789";
+  var arr_cpt = 0;
+  var result = "";
+  var char = arr[arr_cpt]
+  while (char != undefined) {
+    if (char >= 97 && char <= 122) {
+      result += lowerCaseLetters[char - 97];
+    }
+    else if (char >= 65 && char <= 90) {
+      result += upperCaseLetters[char - 65];
+    }
+    else if (char >= 48 && char <= 57) {
+      result += numericLetters[char - 48];
+    }
+    else if (char == 32) {
+      result += " ";
+    }
+    arr_cpt++;
+    char = arr[arr_cpt];
+  }
+  return result;
 }
